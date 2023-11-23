acf8830e6233c9b31a73cc5f77dd11ac96d829d0
rodrigo debian <correo.rodrigo@gmail.com>
2023-11-23 09:29:06 -0500

    setting up settings

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,58 @@
 use actix_web::{
-    get, post, web, App, HttpResponse, HttpServer, Responder,
+    get, App, HttpResponse, HttpServer, Responder,
 };
+use askama::Template;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::env;
+use std::path::{Path};
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct Settings {
+   environment: String,
+   hosting: Hosting  
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct Hosting {
+   ip: String,
+   port: u16
+}
+
+#[derive(Template)]
+#[template(path = "_layout.html")]
+struct LayoutTemplate<'a> {
+    name: &'a str,
+}
 
 #[get("/")]
-async fn hello() -> impl Responder {
+async fn index() -> impl Responder {
+    let hello = LayoutTemplate { name: "Contenido" };    
     HttpResponse::Ok()
-    .body("¡Hola Mundo!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+    .body(hello.render().unwrap())
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let current_dir = env::current_dir()?;
+    let settings_file = Path::new("settings/dev.json");
+
+    let settings_path = current_dir.join(settings_file);
+    
+    let file = File::open(settings_path).expect("Unable to open settings file");
+    let settings: Settings = serde_json::from_reader(file).expect("settings JSON was not well-formatted");
+
+    println!("server is now running");
+    println!("http://{}:{}", settings.hosting.ip, settings.hosting.port);
+
+
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(index)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((settings.hosting.ip, settings.hosting.port))?
     .run()
     .await
 }
