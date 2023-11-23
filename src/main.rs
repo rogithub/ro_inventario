c468b8f823cb30b4fb655a802a3306461aca2053
ro mac mini <correo.rodrigo@gmail.com>
2023-11-23 13:11:21 -0500

    logging crates

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use actix_web::{
 };
 use actix_web::middleware::Logger;
 use env_logger::Env;
+use log::{info};
 
 mod settings;
 mod controllers;
@@ -12,8 +13,8 @@ async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
     let settings = settings::load().expect("Could not load settings file");
-    println!("server running at");
-    println!("http://{}:{}", settings.hosting.ip, settings.hosting.port);
+    info!("server running at");
+    info!("http://{}:{}", settings.hosting.ip, settings.hosting.port);
 
     HttpServer::new(|| {
         App::new()
