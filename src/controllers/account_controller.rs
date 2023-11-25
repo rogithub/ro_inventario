527d8ec24b037259328e01c894f64bdb61c4a1d9
rodrigo debian <correo.rodrigo@gmail.com>
2023-11-25 09:46:04 -0500

    validaciones basicas

--- src/controllers/account_controller.rs
+++ src/controllers/account_controller.rs
@@ -1,17 +1,23 @@
 use askama_actix::{ TemplateToResponse };
 use actix_web::{ web, get, post, Responder };
 use log::{info};
-use crate::models::account_models::{ LoginModel };
+use crate::models::account_models::{ LoginModel, Validate };
 
 
 #[get("/login")]
 pub async fn login() -> impl Responder {
-    let model = LoginModel { email: "".to_string(), password: "".to_string() };    
+    let model = LoginModel::default();    
     model.to_response()
 }
 
 #[post("/submit")]
-pub async fn submit(form: web::Form<LoginModel>) -> impl Responder {
+pub async fn submit(form: web::Form<LoginModel>) -> impl Responder { 
+    let model = LoginModel { 
+        email: form.email.clone(), 
+        password : form.password.clone()
+    };
+    let errors = model.run();   
     info!("{:?}", form);
+    info!("{:?}", errors);
     form.to_response()
 }
--- src/models/account_models.rs
+++ src/models/account_models.rs
@@ -1,10 +1,84 @@
 use askama_actix::{ Template };
 use serde::Deserialize;
+use std::collections::HashMap;
+use regex::Regex;
 
-
-#[derive(Template, Deserialize, Debug)]
+#[derive(Template, Deserialize, Debug, Default)]
 #[template(path = "login.html")]
 pub struct LoginModel {
     pub email:  String,
     pub password:  String
+}
+
+pub trait Validate {
+    fn run(&self) -> HashMap<String, String>;    
+}
+
+fn validate_email(email_address: &str) -> bool {
+    let email_regex = Regex::new(r"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$").unwrap();
+    let is_valid = email_regex.is_match(email_address);
+    is_valid
+}
+
+/// We only validate min length 8 max length 255
+fn validate_password(pwd: &str) -> bool {
+    let pwd_regex = Regex::new(r"^.{8,255}$").unwrap();
+    let is_valid = pwd_regex.is_match(pwd);
+    is_valid
+}
+
+ 
+impl Validate for LoginModel {
+    fn run(&self) -> HashMap<String, String> {
+        let mut errors: HashMap<String, String> = HashMap::new();
+        if !validate_email(&self.email) {
+            errors.insert
+            (
+                "email".to_string(),
+                "El correo electrónico no es válido".to_string()
+            );
+        }
+        if !validate_password(&self.password) {
+            errors.insert
+            (
+                "password".to_string(),
+                "El password dete tener mínimo 8 carácteres de longitud".to_string()
+            );
+        }
+
+        errors
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_string_is_invalid_email() {
+        let is_valid = validate_email("");
+        assert_eq!(is_valid, false);
+    }
+
+    #[test]
+    fn email_can_contain_dots() {
+        let is_valid = validate_email("s.o.s@sos.org");
+        assert_eq!(is_valid, true);
+    }
+
+    #[test]
+    fn password_complexity_min_length() {
+        let is_valid = validate_password("");
+        assert_eq!(is_valid, false);
+
+        let is_valid = validate_password("len");
+        assert_eq!(is_valid, false);
+
+        let is_valid = validate_password("Tres0!");
+        assert_eq!(is_valid, false);
+
+        let is_valid = validate_password("Tres0!78");
+        assert_eq!(is_valid, true);
+    }
 }
