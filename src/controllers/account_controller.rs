8bf4f2719557736374725cacf81391381343d789
rodrigo debian <correo.rodrigo@gmail.com>
2023-11-27 09:37:23 -0500

    auth

--- src/controllers/account_controller.rs
+++ src/controllers/account_controller.rs
@@ -1,7 +1,13 @@
 use askama_actix::{ TemplateToResponse };
-use actix_web::{ web, get, post, Responder };
+use actix_web::{ web, get, post, Responder, HttpRequest };
 use log::{info};
 use crate::models::account_models::{ LoginModel, Validator };
+use actix_web::{
+    HttpMessage as _
+};
+
+
+use actix_identity::{Identity};
 
 
 #[get("/login")]
@@ -11,8 +17,11 @@ pub async fn login() -> impl Responder {
 }
 
 #[post("/submit")]
-pub async fn submit(mut form: web::Form<LoginModel>) -> impl Responder {    
+pub async fn submit(mut form: web::Form<LoginModel>, req: HttpRequest) -> impl Responder {    
     form.validate();  
     info!("{:?}", form);
+
+    Identity::login(&req.extensions(), "user1".to_owned()).unwrap();
+
     form.to_response()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use actix_web::{
-    App, HttpServer, web
-};
+
 use actix_web::middleware::Logger;
 use env_logger::Env;
 use log::{info};
@@ -11,15 +9,42 @@ mod controllers;
 mod models;
 mod validators;
 
+use actix_identity::IdentityMiddleware;
+use actix_session::{config::PersistentSession, storage::CookieSessionStore, SessionMiddleware};
+use actix_web::{
+    cookie::{time::Duration, Key},   
+    middleware, web, App, HttpServer
+};
+
+const ONE_MINUTE: Duration = Duration::minutes(1);
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
     env_logger::init_from_env(Env::default().default_filter_or("info"));
+    
+    // Generate a random secret key. Note that it is important to use a unique
+    // secret key for every project. Anyone with access to the key can generate
+    // authentication cookies for any user!
+    //
+    // If the secret key is read from a file or the environment, make sure it is generated securely.
+    // For example, a secure random key (in base64 format) can be generated with the OpenSSL CLI:
+    // ```
+    // openssl rand -base64 64
+    // ```
+    //
+    // Then decoded and used converted to a Key:
+    // ```
+    // let secret_key = Key::from(base64::decode(&private_key_base64).unwrap());
+    // ```
+    let secret_key = Key::generate();
+
 
     let settings = settings::load().expect("Could not load settings file");
     info!("server running at");
     info!("{}://{}:{}", settings.hosting.protocol, settings.hosting.ip, settings.hosting.port);
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
 
         let account = web::scope("/account")
                         .service(controllers::account_controller::login)
@@ -28,11 +53,24 @@ async fn main() -> std::io::Result<()> {
         App::new()            
             // routes starting by
             .service(controllers::index)            
-            .service(account)
-
-            .wrap(Logger::default())
-            .wrap(Logger::new("%a %{User-Agent}i"))            
+            .service(account)            
             .service(Files::new("/", "./wwwroot").prefer_utf8(true))
+
+            // cookie auth & identity
+            .wrap(IdentityMiddleware::default())
+            .wrap(
+                SessionMiddleware::builder(CookieSessionStore::default(), secret_key.clone())
+                    .cookie_name("auth-inventario".to_owned())
+                    .cookie_secure(false)
+                    .session_lifecycle(PersistentSession::default().session_ttl(ONE_MINUTE))
+                    .build(),
+            )
+
+
+            // logger
+            .wrap(middleware::NormalizePath::trim())
+            .wrap(middleware::Logger::default())            
+            .wrap(Logger::new("%a %{User-Agent}i"))            
             
     })
     .bind((settings.hosting.ip, settings.hosting.port))?
