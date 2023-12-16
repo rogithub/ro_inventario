b5bce6ade7bafff8696245b022fc0ccbe4b7f789
rodrigo debian <correo.rodrigo@gmail.com>
2023-12-16 13:04:30 -0500

    landing page

--- src/app_state/mod.rs
+++ src/app_state/mod.rs
@@ -1,14 +1,42 @@
 
 use sqlx::{SqlitePool};
 use crate::settings::model::{Settings};
+use actix_identity::{Identity};
+use log;
+
+#[derive(Debug)]
+pub enum IdentityError {
+    MissingId,
+    InvalidId(String),
+}
 
 #[derive(Clone)]
 pub struct AppState {
     pub settings: Settings,
 }
 
+#[derive(Debug, Default)]
+pub struct IdenityClaims {
+    is_authenticated: bool,
+    email: String
+}
+
 impl AppState {
     pub async fn connect(&self) -> SqlitePool {
         SqlitePool::connect(&self.settings.db_url()).await.unwrap()
     }
+
+    pub fn identity_check(identity: Option<Identity>) -> Result<IdenityClaims, IdentityError> {
+        match identity.map(|id| id.id()) {
+            None => {
+                log::warn!("Missing user identity");
+                Ok(IdenityClaims { is_authenticated: false, email: "".to_string() })
+            },
+            Some(Ok(id)) => Ok(IdenityClaims { is_authenticated: true, email: id }),
+            Some(Err(err)) => {
+                log::error!("Invalid user identity: {}", err.to_string());
+                Err(IdentityError::InvalidId(err.to_string()))
+            },
+        }
+    }    
 }
--- src/controllers/home_controller.rs
+++ src/controllers/home_controller.rs
@@ -1,21 +1,33 @@
 
 use askama_actix::{ TemplateToResponse };
-use actix_web::{ get,  error, Responder };
+use actix_web::{ get, Responder };
 use crate::models::home_models::{ LandingModel };
-
 use actix_identity::{Identity};
+use actix_web::web;
+use crate::app_state::AppState;
 
 
 #[get("/landing")]
-pub async fn landing(identity: Option<Identity>) -> impl Responder {
-    let id = match identity.map(|id| id.id()) {
-        None => "anonymous".to_owned(),
-        Some(Ok(id)) => id,
-        Some(Err(err)) => return Err(error::ErrorInternalServerError(err)),
-    };
+pub async fn landing(identity: Option<Identity>, data: web::Data<AppState>) -> impl Responder {
+    AppState::identity_check(identity).unwrap();
 
-    let model = LandingModel::default();    
-    Ok(model.to_response())
-
-    //Ok(format!("Hello {id}"))
+    let model = LandingModel::default();
+    model.to_response()
+/*
+    match AppState::identity_check(identity) {
+        Ok(claims) => {
+            // use claims for authorization or other purposes
+            let model = LandingModel::default();
+            Ok(model.to_response())
+        },
+        Err(IdentityError::MissingId) => {
+            log::warn!("Missing user identity");
+            Err(actix_web::error::Unauthorized()) // or custom unauthorized response
+        },
+        Err(IdentityError::InvalidId(err)) => {
+            log::error!("Invalid user identity: {}", err);
+            Err(actix_web::error::InternalServerError::from(err)) // or custom error response
+        },
+    }
+*/
 }
