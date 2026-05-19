2def9f3161cd506bc061b2d17d15616af5500dd8
Rodrigo <correo.rodrigo@gmail.com>
2026-05-19 06:13:03 -0500

    restarting from scratch

--- src/models/home_models.rs
+++
@@ -1,7 +0,0 @@
-use askama_actix::{ Template };
-use serde::Deserialize;
-
-#[derive(Template, Deserialize, Debug, Default)]
-#[template(path = "landing.html")]
-pub struct LandingModel {    
-}
--- src/models/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod account_models;
-pub mod home_models;
